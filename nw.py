6bfe7260cb07cf9757c94d27566076b6a74e1303
Ngwa Jude <ngwajude161@gmail.com>
2021-09-03 10:33:53 +0100

    down payment if statement

--- nw.py
+++ nw.py
@@ -29,8 +29,40 @@ print(course.lower())
 print(course.title())
 print(course.find('P')) #returns index
 print(course.replace('P', 'J')) #replace method replaces a word or letter in a string
-print('Python' in course) #in operator returns a boolean value ''')
+print('Python' in course) #in operator returns a boolean value 
 
-#arithmetic operations
+#math functions
+import math
+print(math.ceil(2.9))
+print(math.flooor(2.9))
+x  =2.9
+print(round(x))
+print(abs(-2.9)) 
+
+#if statements
+Is_hot = True
+is_cold = True
+if Is_hot:
+    print("its a hot day")
+    print('drink plenty of water')
+elif is_cold:
+    print('its a cold day')
+    print('wear warm clothes')
+else:
+    print("its alovely day") ''')
+
+Has_good_credit =True
+price = 1000000
+if Has_good_credit:
+    down_payment = int(0.1 * price)
+    print(f"your down payment is ${down_payment}")
+else:
+    down_payment = int(0.2 * price)
+    print(f"your down payment is ${down_payment}")
+    
+
+
+
+
   
 
