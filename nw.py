f1fd56b8218ee47fa124006a772ab45087e3fd51
Ngwa Jude <ngwajude161@gmail.com>
2022-02-13 04:28:13 +0100

    new adjustment and pickup

--- nw.py
+++ nw.py
@@ -1,10 +1,11 @@
+'''
 print("Ngwa jude")
 print("o----")
 print("|||||")
 print("*" * 10)
 
 #input
-('''birth_year = int(input('whats your birth year? '))
+birth_year = int(input('whats your birth year? '))
 print(type(birth_year))
 age = 2021 - birth_year
 print(type(age))
@@ -59,14 +60,64 @@ if Has_good_credit:
 else:
     down_payment = int(0.2 * price)
 print(f"your down payment is ${down_payment}") 
-''')
+
 
 #comparison operator
- name = 'Ngwa Jude'
- if len(name) < 3:
+name = 'Ngwa Jude'
+if len(name) < 3:
      print("name must be 3 characters")
-eleif len(name) > 50:
+elif len(name) > 50:
     print('name can be 50 characters max')
 else:
     print('name loooks good')
+
+
+
+old = int(input("enter old meter reading "))
+new = int(input("enter new mater reading "))
+vat = 600
+tax = 50
+unit = new - old
+unit_consumption = unit * vat
+if unit > 7:
+    price =  unit * tax
+    print(price)
+   
+
+def area_of_trapezium(a,b,h):
+    area = 0.5 * (a + b) * h
+    print (f"area of trapezium{area}")
+def main():
+    area_of_trapezium(10, 15, 20)
+
+if __name__ == "__main__":
+    main()
+
+test_varaible = 5
+def outer_function():
+    test_varaible = 10
+    def inner_function():
+            test_varaible =100
+            print(f"local variable of {test_varaible} having inner function ")  
+    inner_function()
+    print(f"local varaible of {test_varaible} having outer function")
+outer_function()
+print(f"global variable of {test_varaible}")
+
+def combined_area(a,b):
+    def area_of_cube(x):
+        return 6*pow(x,2)
+    return area_of_cube(a)+ area_of_cube(b)
+def main():
+    result=combined_area(2,3)
+    print(f"{result}")
+
+if __name__ == "__main__":
+    main()
+'''
+i = 0
+while i<10:
+    i = i+1
+    print(i)
+    
 
