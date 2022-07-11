246a7a2624c0d9b1b9d985218388e70343b19385
jude <ngwajude 161@gmail.com>
2022-07-11 09:30:30 +0100

    working with variables and strings

---
+++ nw.py
@@ -0,0 +1,16 @@
+#working with varaibles
+message="you are useless"
+
+message="reall use less"
+print(message)
+
+#working with strings
+first_name="ada lovelace"
+last_name ="Jude"
+print(first_name.title()) #using the .title methiod to change the case of the case
+print(first_name.upper()) #changinr the entire first_name to upper case
+print(first_name.lower()) #Changing all to lowe case
+
+print(f"{first_name} {last_name.title()}") #using formated string 
+
+print("languages: Python \n java \n c\n R") #Using the escape character \n to move to a new line
