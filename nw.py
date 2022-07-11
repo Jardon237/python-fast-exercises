b5b968c24e8bdeb960a38ad25d3e7278f0295fb3
jude <ngwajude 161@gmail.com>
2022-07-11 09:49:29 +0100

    exercise 1

--- nw.py
+++ nw.py
@@ -14,3 +14,20 @@ print(first_name.lower()) #Changing all to lowe case
 print(f"{first_name} {last_name.title()}") #using formated string 
 
 print("languages: Python \n java \n c\n R") #Using the escape character \n to move to a new line
+
+from email.policy import strict
+
+
+name ="Eric"
+print(f"Hello {name} would you like to learn some python today?")
+print(name.title())
+print(name.upper())
+print(name.lower()) 
+
+famous_person = "Albert Einstein"
+print(f"{famous_person} once said, 'A person who never made a mistake never tried anything new'")
+stripping =" Jude "
+print(f"name:\n {stripping}")
+print(stripping.rstrip())
+print(stripping.strip())
+print(stripping.lstrip())
