de3aea1fa0e93cc8ad205cdc7ccfdf4758abedab
jude <ngwajude 161@gmail.com>
2022-07-22 18:29:49 +0100

    working with inputs

---
+++ while.py
@@ -0,0 +1,14 @@
+#using input when recieving data
+car =input("what type of car do u want ")
+print(f"let me see if i can find you a {car}")
+
+people =int(input("how many poeple are in their dinner room? "))
+if people > 8:
+    print("wait for a free table")
+else:
+    print("your table is ready")
+number= int(input("enter a number "))
+if number % 10 == 0:
+    print("number is a multiple of 10")
+else:
+    print("number is not a multiple of 10")
