2d6e20251455f16509dc5f6b629c64cd293fa092
jude <ngwajude 161@gmail.com>
2022-07-25 15:47:39 +0100

    working with functions

---
+++ functions.py
@@ -0,0 +1,8 @@
+def display_message():
+    print("i am learning about functions")
+display_message()    
+
+def favorite_book(tittle):
+    print(f"One of my favorit book is {tittle.title()}")
+favorite_book('alice')
+
