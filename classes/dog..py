df4b4ee87c373bf069a4b0ba28c1764bb41d74b9
jude <ngwajude 161@gmail.com>
2022-08-05 11:42:36 +0100

    started working with classes. created a multiple instance of a class

---
+++ classes/dog..py
@@ -0,0 +1,22 @@
+
+class Dog:
+
+    def __init__(self, name, age):
+          self.name = name
+          self.age = age #attribute
+
+    def sit(self):
+        print(f"{self.name} is now sitting.") #methods
+
+    def roll_over(self):
+        print(f"{self.name} rolled over!")
+
+my_dog=Dog("will", 6)
+your_dog=Dog("jake", 3) #instance
+print(f"my dog is called {my_dog.name}")
+print(f"it is {my_dog.age}")
+my_dog.sit() #calliung back the method
+
+print(f"your dog is called{your_dog.name}")
+print(f"it is {your_dog.age}")
+your_dog.roll_over() #calling abck the method
