7a7004b9f466e701ceda4512b9a485c132a66143
Jardon237 <ngwajude161@gmail.com>
2022-10-22 03:56:23 +0100

    modified restuarant class

--- classes/restaurant.py
+++ classes/restaurant.py
@@ -1,25 +1,32 @@
-from cgitb import small
-from os import PRIO_PGRP
-from pydoc import describe
-
-
 class Restuarant:
-    def __init__(self,name,type):
+
+    def __init__(self,name,type,):
         self.name = name
         self.type = type
+        self.number_served = 0
 
     def describe_restuarant(self):
         print(f"welcome to {self.name.upper()} restuarant" )
         print(f"we have {self.type.title()} cusine available for today")
 
     def open_restuarant(self):
-        print("we open our doord everydayu from 8am to 5pm")
+        print("we open our door everydayu from 8am to 5pm")
+
+    def restuarant(self):
+        print(f"number of customers serverd are{self.number_served}")
+        
+    def increment_number_served(self, plus):
+        self.number_served += plus
+
 
 big_restaurant =Restuarant("philip_lounge", "frech")
 small_restaurant=Restuarant("janna", "affrican")
 big_restaurant.describe_restuarant()
 big_restaurant.open_restuarant()
 small_restaurant.describe_restuarant()
+big_restaurant.number_served = 6
+big_restaurant.restuarant()
+big_restaurant.increment_number_served(6)
 
 class User:
     def __init__(self, first_name, last_name):
